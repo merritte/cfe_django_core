b45d71a6f3d560a871059b91a043257cd4f086fb
Shay-Jahen Merritte <Shay@Merritte.com>
2014-12-27 23:26:02 -0600

    added contact page

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from .forms import ContactForm
 # Create your views here.
 
 
 def home(request):
+	form = ContactForm(request.POST or None)
+
+	if form.is_valid():
+		print form.cleaned_data['email']
+		#print request.POST
+
 	context = locals()
 	template = "contact.html"
 	return render(request, template, context)
